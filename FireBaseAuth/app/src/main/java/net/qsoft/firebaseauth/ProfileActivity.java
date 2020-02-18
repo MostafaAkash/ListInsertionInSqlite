96a9c66e10f0f0de22bd97cf11555daa0e1d6e7e
Shakif Ferdous <shakifferd11@gmail.com>
2020-02-18 17:57:52 +0600

    Authentication using mail and password

---
+++ FireBaseAuth/app/src/main/java/net/qsoft/firebaseauth/ProfileActivity.java
@@ -0,0 +1,14 @@
+package net.qsoft.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
